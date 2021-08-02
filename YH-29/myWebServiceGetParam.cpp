ec5f5895f71dcfdeaf6b3e58d270fa2e9e893937
yuanhui360 <85416816+yuanhui360@users.noreply.github.com>
2021-08-02 13:48:50 -0400

    Create myWebServiceGetParam.cpp

---
+++ YH-29/myWebServiceGetParam.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <restbed>
+
+using namespace std;
+using namespace restbed;
+
+void get_method_handler( const shared_ptr< Session > session )
+{
+    //perform awesome solutions logic...
+    const auto request = session->get_request();
+
+    string response_body;
+    std::string paramLevel, paramGender;
+    std::stringstream myStream;
+    paramLevel = request->get_query_parameter("level");
+    paramGender = request->get_query_parameter("gender");
+    myStream << "{\r\n";
+    myStream << "    'level' : '" << paramLevel << "',\r\n";
+    myStream << "    'gender' : '" << paramGender << "'\r\n";
+    myStream << "}\r\n";
+
+    response_body = myStream.str();
+    session->close( OK, response_body, { { "Content-Length", ::to_string(response_body.length()) }, { "Content-Type", "app
+}
+
+void service_ready_handler( Service& )
+{
+    fprintf( stderr, "The service port 1234 /player is up and running." );
+}
+
+int main( const int, const char** )
+{
+
+    auto resource = make_shared< Resource >( );
+    resource->set_path( "/player" );
+    resource->set_method_handler( "GET", get_method_handler );
+
+    auto settings = make_shared< Settings >( );
+    settings->set_port( 1234 );
+    settings->set_default_header( "Connection", "close" );
+
+    // Service service;
+    auto service = make_shared< Service >( );
+    service->publish( resource );
+    service->set_ready_handler( service_ready_handler );
+    service->start( settings );
+
+    return EXIT_SUCCESS;
+}

ee521625198749e41b5bc756fcd37d736490e15a
yuanhui360 <85416816+yuanhui360@users.noreply.github.com>
2021-08-31 14:01:28 -0400

    Update my_get.cpp

--- YH-109/my_get.cpp
+++ YH-109/my_get.cpp
@@ -1,24 +1,31 @@
 #include <AMQPcpp.h>
+#include <string>
 
 using namespace std;
 
 int main (int argc, char* argv[]) {
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cout << "Usage: my_get <queue>" << std::endl;
+        std::cout << "Usage: my_get <host:port> <queue>" << std::endl;
+        std::cout << "Example :" << std::endl;
+        std::cout << "my_get \"192.168.0.106:5672\"  my_queue" << std::endl;
         return 1;
     }
 
-    char const *queue_name = argv[1];
+    char const *queue_name = argv[2];
+    std::string conn_str = "test:test@";
+    conn_str += std::string(argv[1]);
+    conn_str += std::string("/my_vhost");
 
     try {
 
         /*
          *  amqp://[$USERNAME[:$PASSWORD]\@]$HOST[:$PORT]/[$VHOST]
+         *  AMQP amqp("test:test@192.168.0.106:5672/my_vhost");
          */
 
-        AMQP amqp("test:test@localhost:5672/my_vhost");
+        AMQP amqp(conn_str);
 
         AMQPQueue * qu2 = amqp.createQueue(queue_name);
         qu2->Declare();

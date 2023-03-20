aa63df61e86d3c57094734525d8fb9c16cb9515b
yuanhui360 <85416816+yuanhui360@users.noreply.github.com>
2023-03-20 19:15:56 -0400

    Create EchoGetPostWS.h

---
+++ YH-131/EchoGetPostWS.h
@@ -0,0 +1,25 @@
+#ifndef ECHOGETPOSTWS_H
+#define ECHOGETPOSTWS_H
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/basic/kvp.hpp>
+#include <bsoncxx/json.hpp>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/client_session.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
+
+#include <restbed>
+#include <boost/json.hpp>
+#include <boost/json/basic_parser_impl.hpp>
+
+#endif // ECHOGETPOSTWS_H

6f57a99e9b829a5566814b9754a55101d3744a53
yuanhui360 <85416816+yuanhui360@users.noreply.github.com>
2024-05-30 12:57:46 -0400

    Create evhttp_http_server.cpp

---
+++ YH-140/evhttp_http_server.cpp
@@ -0,0 +1,102 @@
+#include <iostream>   // std::cout
+#include <string>     // std::string, std::stoi
+
+#include <string.h>
+#include <signal.h>
+#include <evhttp.h>
+#include <event2/buffer.h>
+#include <event2/event.h>
+#include <event2/http.h>
+
+/*
+ * /usr/include/event2/http.h
+ */
+
+static void my_request_reply(struct evhttp_request *req, void *ctx)
+{
+    /*
+     *   1) Allocate ev_buffer for reply
+     *   2) GET and Parse request URI (cmd type, host)
+     *   3) write HTML reply  to buffer
+     *   4) send HTTP reply
+     *   5) free allocated memory
+     */
+
+    /*
+     "<!DOCTYPE html>\n"
+     "<html lang=\"en\">"
+     "<head>\n"
+     "</head>\n"
+     "<body>\n"
+     "<div style=\"font-size: 36px; font-weight: bold; text-align: center; color: blue;\">\n"
+     "Hui Test Libevent HTTP Server\n"
+     "</div>\n"
+     "</body>\n"
+     "</html>\n");
+     */
+
+}
+
+static void my_signal_event_cb(evutil_socket_t fd, short event, void *arg)
+{
+    /*
+     *  Signal event SIGINT call back function
+     *  SIGINT <-- Interrupt from keyboard
+     *  1) Parse, decoding argument
+     *  2) event_base_loopbreak()
+     */
+    struct event_base *base = (struct event_base *) arg;
+    printf("%s signal received\n", strsignal(fd));
+    event_base_loopbreak(base);
+}
+
+int main(int argc, char *argv[])
+{
+
+    if ( argc != 2 ) {
+        printf("usage: %s <port>\n", argv[0]);
+        printf("Example : %s 1234\n", argv[0]);
+        return 1;
+    }
+
+    /*
+     *    Header file : /sur/include/http.h
+     *
+     *    1) Create a new event base           : function event_base_new()
+     *    2) Create a new HTTP server          : function evhttp_new()
+     *    3) Binds an HTTP server on the       : function evhttp_bind_socket()
+     *       specified address and port.
+     *    4) Set a callback for all requests   : function evhttp_set_gencb()
+     *       that are not caught by specific
+     *       callbacks
+     *    5) Create signal event ctrl+C exit   : function evsignal_new()
+     *    6) Add signal event to base          : function event_add()
+     *    7) Make an event active              : function event_base_dispatch()
+     *
+     *  End :
+     *    8) free http server                  : function evhttp_free()
+     *    9) free signal event                 : function event_free()
+     *   10) free event base                   : function event_base_free()
+     */
+
+     char http_addr[] = "0.0.0.0";
+     ev_uint16_t http_port = (ev_uint16_t) std::stoi(argv[1]);
+
+     struct event_base *base = event_base_new();            // Create a new event base
+     struct evhttp *http_server = evhttp_new(base);         // Create a new HTTP server
+
+     int http_handle = evhttp_bind_socket(http_server, (const char *) http_addr, http_port);
+     evhttp_set_gencb(http_server, my_request_reply, NULL);  // Set callback for all requests
+
+     struct event *sig_int = evsignal_new(base, SIGINT, my_signal_event_cb, base);
+     event_add(sig_int, NULL);
+
+     printf("Listening request on http://%s:%d (%d)\n", http_addr, http_port, http_handle);
+     event_base_dispatch(base);
+
+     evhttp_free(http_server);
+     event_free(sig_int);
+     event_base_free(base);
+     return 0;
+
+}
